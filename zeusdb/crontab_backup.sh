74c6a9c87ab257e8366a695301168675d7bba3cd
oylbin <oylbin@gmail.com>
2011-12-08 19:14:16 +0800

    x

--- zeusdb/crontab_backup.sh
+++ zeusdb/crontab_backup.sh
@@ -4,8 +4,10 @@ folders="main.tcb.8888"
 
 export SCRIPTDIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+date +"%Y-%m-%d %H:%M:%S"
 for folder in $folders
 do
     $SCRIPTDIR/db backup $folder
-    cd $folder/backup && find . -type f -mtime +5 -exec rm {} \;
+    cd $SCRIPTDIR/$folder/backup && find . -type f -mtime +5 -exec rm {} \;
 done
+date +"%Y-%m-%d %H:%M:%S"
